bf5d1a2d9ba7a750de30b7288e9a7474f6cfef65
Vlad Dumitrescu <vladdu55@gmail.com>
2019-05-25 22:53:31 +0200

    install tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
         >vue-cli documentation</a
       >.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3 class="bg-red-500">Installed CLI Plugins</h3>
     <ul>
       <li>
         <a
@@ -104,19 +104,4 @@ export default class HelloWorld extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>

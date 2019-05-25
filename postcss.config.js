d42cd7c9acfd4f4f39b317e88760b0926c0934fa
Vlad Dumitrescu <vladdu55@gmail.com>
2019-05-26 00:13:16 +0200

    install purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const postcss = require("postcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    process.env.NODE_ENV === "production"
+      ? postcss([
+          purgecss({
+            content: ["./src/**/*.html", "./src/**/*.vue"],
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || []
+          })
+        ])
+      : ""
+  ]
 };
